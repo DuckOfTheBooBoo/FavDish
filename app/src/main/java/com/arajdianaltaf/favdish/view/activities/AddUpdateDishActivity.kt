15b8dc38be4c743e05e0545ba2bcd0c7fe4cc716
Arajdian Altaf <arajdianaltaf123@gmail.com>
2022-12-21 21:18:31 +0700

    ViewBinding, Setup Action Bar in AddUpdateDishActivity.kt and onclick listener

--- app/src/main/java/com/arajdianaltaf/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/arajdianaltaf/favdish/view/activities/AddUpdateDishActivity.kt
@@ -2,11 +2,44 @@ package com.arajdianaltaf.favdish.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.arajdianaltaf.favdish.R
+import com.arajdianaltaf.favdish.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var myBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+
+        myBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+
+        setContentView(myBinding.root)
+
+        setupActionBar()
+
+        myBinding.ivAddDishImage.setOnClickListener(this)
     }
+
+    private fun setupActionBar() {
+        setSupportActionBar(myBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        myBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null){
+            when (v.id) {
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(this, "You have clicked the ImageView", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+        }
+    }
+
 }
